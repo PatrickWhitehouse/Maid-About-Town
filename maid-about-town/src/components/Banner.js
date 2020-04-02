e6ffd5b66a7cf8a762cac2a588993ff82e042fa0
Patrick Whitehouse <patrick.whitehouse@pushon.co.uk>
2020-04-02 22:34:05 +0100

    lazy load banner

--- maid-about-town/src/components/Banner.js
+++ maid-about-town/src/components/Banner.js
@@ -2,32 +2,34 @@ import React from 'react'
 import { graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 const Banner = ({ title, subtitle, image }) => {
-    const patrickQuery = useStaticQuery(graphql`
+    const bannerImageQuery = useStaticQuery(graphql`
 query MyQuery {
-  allImageSharp {
+  allFile {
     edges {
       node {
-        original {
-          src
+        id
+        childImageSharp {
+          original {
+            src
+          }
+          fluid {
+             ...GatsbyImageSharpFluid
+          }
         }
       }
     }
   }
 }
+`)
 
-    `)
-    const myTest = patrickQuery.allImageSharp.edges.find(e => e.node.original.src === image);
-    console.log(myTest);
-    console.log(patrickQuery);
-
-
+    const findImage = bannerImageQuery.allFile.edges.find(({ node }) => node.childImageSharp.original.src === image);
     return (
         <div className="banner relative flex justify-start items-center px-10">
             <div className="banner__content mt-10 relative z-10 text-4xl text-white">
                 <span className="block title">{title}</span>
                 <span className="block subtitle">{subtitle}</span>
             </div>
-            <img src={image} alt={title} className="w-full h-full absolute top-0 left-0 object-cover z-0" />
+            <Img fluid={findImage.node.childImageSharp.fluid} alt={title} className="w-full h-full absolute top-0 left-0 object-cover z-0" style={{ position: "absolute" }} />
         </div >
     )
 }
--- maid-about-town/src/pages/index.js
+++ maid-about-town/src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Banner from '../components/Banner';
-import logo from '../images/banner.jpg'
+import bannerImage from '../../static/banner.jpg';
 
 const Index = () => {
     return (
         <Layout>
-            <Banner title="Not enough hours in the day?" subtitle="We will take care of keeping your home clean, so you don't have to." image={logo} />
+            <Banner title="Not enough hours in the day?" subtitle="We will take care of keeping your home clean, so you don't have to." image={bannerImage} />
         </Layout>
     )
 }
